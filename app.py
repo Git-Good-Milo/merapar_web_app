51a7489fa9a2ad75ea52026c6e34f82735fe7342
Miles Eastwood <milotheengineer@gmail.com>
2022-10-01 12:56:52 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/dynamic_string/<name>")
+def dyn_string(name):
+    return f"<h1>The saved string is {escape(name)}</h1>"
